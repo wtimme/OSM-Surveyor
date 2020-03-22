1b9ee4def260b6394c33e23cd72ee129a07eac10
wtimme <wtimme@users.noreply.github.com>
2020-03-22 21:11:35 +0100

    Add `BoundingBox` struct
    This model represents a geo area.

---
+++ OSMSurveyorFramework/OSM/BoundingBox.swift
@@ -0,0 +1,14 @@
+//
+//  BoundingBox.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 3/22/20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+struct BoundingBox {
+    let minimum: Coordinate
+    let maximum: Coordinate
+}
