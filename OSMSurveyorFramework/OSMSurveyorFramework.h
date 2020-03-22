8c3a4b9e3996e7e625dd7d6dd6a3b073f931c3c4
wtimme <wtimme@users.noreply.github.com>
2020-03-22 20:46:41 +0100

    Add "OSMSurveyorFramework" and a corresponding test target
    This framework will contain the logic of the application.

---
+++ OSMSurveyorFramework/OSMSurveyorFramework.h
@@ -0,0 +1,19 @@
+//
+//  OSMSurveyorFramework.h
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 3/22/20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OSMSurveyorFramework.
+FOUNDATION_EXPORT double OSMSurveyorFrameworkVersionNumber;
+
+//! Project version string for OSMSurveyorFramework.
+FOUNDATION_EXPORT const unsigned char OSMSurveyorFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OSMSurveyorFramework/PublicHeader.h>
+
+
