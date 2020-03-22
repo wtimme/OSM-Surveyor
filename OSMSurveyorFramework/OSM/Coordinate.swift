118f92c9bcc28702303c168ff10f283c20fcf009
wtimme <wtimme@users.noreply.github.com>
2020-03-22 21:10:24 +0100

    Add `Coordinate` struct
    This model represents a geo location.

---
+++ OSMSurveyorFramework/OSM/Coordinate.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinate.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 3/22/20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+struct Coordinate {
+    let latitude: Double
+    let longitude: Double
+}
