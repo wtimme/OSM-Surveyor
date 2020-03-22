4940412f2ff450240b3ec6774652546eb279d6f5
wtimme <wtimme@users.noreply.github.com>
2020-03-22 17:50:11 +0100

    Add empty "Single View App" Xcode project

---
+++ OSMSurveyor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OSMSurveyor
+//
+//  Created by Wolfgang Timme on 3/22/20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
