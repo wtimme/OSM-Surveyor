893b1c39968c63263fa3cff88d0bc5154792d3e9
wtimme <wtimme@users.noreply.github.com>
2020-03-24 20:44:53 +0100

    Add `MapViewControllerProtocol`

---
+++ OSMSurveyor/Map/MapViewControllerProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewControllerProtocol.swift
+//  OSMSurveyor
+//
+//  Created by Wolfgang Timme on 24.03.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+protocol MapViewControllerProtocol {
+    func fly(to position: CameraPosition, duration: TimeInterval)
+}
