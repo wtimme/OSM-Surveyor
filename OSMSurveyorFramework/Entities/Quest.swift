c92c1af896405f0e6de6bccec2e7627cd0ed8ec4
wtimme <wtimme@users.noreply.github.com>
2020-03-27 16:26:09 +0100

    Add entity `Quest`

---
+++ OSMSurveyorFramework/Entities/Quest.swift
@@ -0,0 +1,63 @@
+//
+//  Quest.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 27.03.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+import SwiftOverpassAPI
+
+/// A `Quest` represents one single task that the mapper can solve.
+struct Quest {
+    enum Status: String {
+        /// just created
+        case new
+        
+        /// user answered the question (waiting for changes to be uploaded)
+        case answered
+        
+        /// user chose to hide the quest
+        case hidden
+        
+        /// The system (decided that it) doesn't show the quest. They may become visible again (-> NEW)
+        case invisible
+        
+        /**
+         The quest has been uploaded (either solved or dropped through conflict). The app needs to
+         remember its solved quests for some time before deleting them because the source the app
+         is pulling it's data for creating quests from (usually Overpass) lags behind the database
+         where the app is uploading its changes to.
+         Note quests are generally closed after upload, they are never deleted
+         */
+        case closed
+        
+        /**
+         The quest has been closed and after that the user chose to revert (aka undo) it. This state
+         is basically the same as CLOSED, only that it will not turn up in the list of (revertable)
+         changes. Note, that the revert-change is done via another Quest upload, this state is only
+         to mark this quest as that a revert-quest has already been created
+         */
+        case revert
+    }
+    
+    /// An auto-incremented ID to identify the quest.
+    let id: Int64
+    
+    /// The quest type. One quest type can have multiple instances of `Quest`.
+    let type: String
+    
+    /// The status of the quest.
+    let status: Status
+    
+    /// The date at which the quest was last updated.
+    /// Can be used, for example, to get a list of recently solved quests.
+    let lastUpdate: Date
+    
+    /// The type of the OSM element that this quest refers to.
+    let elementType: OPElementType
+    
+    /// The ID of the OSM element that this quest refers to.
+    let elementId: Int
+}
