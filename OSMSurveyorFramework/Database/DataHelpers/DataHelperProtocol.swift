b4a016442b032eed9020b5d629a0ce896b3aa5f2
wtimme <wtimme@users.noreply.github.com>
2020-03-27 14:04:34 +0100

    Add the `Connection` to the data helper

--- OSMSurveyorFramework/Database/DataHelpers/DataHelperProtocol.swift
+++ OSMSurveyorFramework/Database/DataHelpers/DataHelperProtocol.swift
@@ -7,10 +7,13 @@
 //
 
 import Foundation
+import SQLite
 
 /// Source: https://masteringswift.blogspot.com/2015/09/create-data-access-layer-with.html
 protocol DataHelperProtocol {
     associatedtype T
+    static var db: Connection? { get set }
+    
     static func createTable() throws -> Void
     static func insert(item: T) throws -> Int64
     static func delete(item: T) throws -> Void
