f0a75c02aa44f0b2d5693a70e23f13efe656b2e5
wtimme <wtimme@users.noreply.github.com>
2020-03-27 17:47:40 +0100

    Add entity `Tile`

---
+++ OSMSurveyorFramework/OSM/Tile.swift
@@ -0,0 +1,14 @@
+//
+//  Tile.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 27.03.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+struct Tile {
+    let x: Int
+    let y: Int
+}
