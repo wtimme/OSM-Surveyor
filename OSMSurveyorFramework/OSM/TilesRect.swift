9e985059d4591cea34b009a5b4223860a4d975bf
wtimme <wtimme@users.noreply.github.com>
2020-03-28 12:31:18 +0100

    Add struct `TilesRect`
    This data structure was taken from StreetComplete.
    Source: https://github.com/westnordost/StreetComplete/blob/master/app/src/main/java/de/westnordost/streetcomplete/util/SlippyMapMath.kt#L41

---
+++ OSMSurveyorFramework/OSM/TilesRect.swift
@@ -0,0 +1,16 @@
+//
+//  TilesRect.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 27.03.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+struct TilesRect {
+    let left: Int
+    let top: Int
+    let right: Int
+    let bottom: Int
+}
