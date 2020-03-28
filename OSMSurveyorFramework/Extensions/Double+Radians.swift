443c06ea84b7c660dcf13609dea3836fea2a9090
wtimme <wtimme@users.noreply.github.com>
2020-03-28 12:26:03 +0100

    Add extension on `Double` to convert degrees to radians and back

---
+++ OSMSurveyorFramework/Extensions/Double+Radians.swift
@@ -0,0 +1,19 @@
+//
+//  Double+Radians.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 27.03.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    func toRadians() -> Double {
+        return self * .pi / 180
+    }
+    
+    func toDegrees() -> Double {
+        return self * 180 / .pi
+    }
+}
