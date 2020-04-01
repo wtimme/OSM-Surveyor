6644dce1f012e7805c18294a46afa2c24bf1530e
wtimme <wtimme@users.noreply.github.com>
2020-04-01 20:34:39 +0200

    Move `CameraPosition` struct to OSMSurveyorFramework

---
+++ OSMSurveyorFramework/CameraPosition.swift
@@ -0,0 +1,23 @@
+//
+//  CameraPosition.swift
+//  OSMSurveyor
+//
+//  Created by Wolfgang Timme on 24.03.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+public struct CameraPosition {
+    public let center: Coordinate
+    public let zoom: Double
+    public let bearing: Double
+    public let pitch: Double
+    
+    public init(center: Coordinate, zoom: Double, bearing: Double, pitch: Double) {
+        self.center = center
+        self.zoom = zoom
+        self.bearing = bearing
+        self.pitch = pitch
+    }
+}
