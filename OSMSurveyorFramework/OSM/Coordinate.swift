bbe3ba851ba40838a2b84776503a51db991f390c
wtimme <wtimme@users.noreply.github.com>
2020-04-01 20:22:49 +0200

    Make initializer of `Coordinate` public

--- OSMSurveyorFramework/OSM/Coordinate.swift
+++ OSMSurveyorFramework/OSM/Coordinate.swift
@@ -11,6 +11,11 @@ import Foundation
 public struct Coordinate {
     public let latitude: Double
     public let longitude: Double
+    
+    public init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
 }
 
 extension Coordinate: Equatable {}
