bbdbe5bea0c5546c3f089013a9ad46d0a2f06fce
wtimme <wtimme@users.noreply.github.com>
2020-04-03 08:50:38 +0200

    Add empty class `QuestProvider`

---
+++ OSMSurveyorFramework/QuestProvider.swift
@@ -0,0 +1,18 @@
+//
+//  QuestProvider.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 03.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+protocol QuestProviding {
+}
+
+class QuestProvider {
+}
+
+extension QuestProvider: QuestProviding {
+}
