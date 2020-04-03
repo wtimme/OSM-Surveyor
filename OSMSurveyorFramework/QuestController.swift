16d1c37d7c6bd5f3aa4767767450fdb6a11d4a32
wtimme <wtimme@users.noreply.github.com>
2020-04-03 08:40:41 +0200

    Add `download` method to `QuestController`

--- OSMSurveyorFramework/QuestController.swift
+++ OSMSurveyorFramework/QuestController.swift
@@ -9,10 +9,34 @@
 import Foundation
 
 protocol QuestControlling {
+    /// Download quests in at least the given bounding box asynchronously. The next-bigger rectangle
+    /// in a (z14) tiles grid that encloses the given bounding box will be downloaded.
+    ///
+    /// - Parameters:
+    ///     - boundingBox: The minimum area to download
+    ///     - maxQuestTypesToDownload: Download at most the given number of quest types. `nil` for unlimited
+    ///     - isPriority: Whether this shall be a priority download (cancels previous downloads and puts itself in the front)
+    func download(boundingBox: BoundingBox, maxQuestTypesToDownload: Int?, isPriority: Bool)
 }
 
 final class QuestController {
+    // MARK: Private properties
+    
+    private let questTileZoom: Int
+    private let downloader: QuestDownloading
+    
+    // MARK: Initializer
+    
+    init(questTileZoom: Int = 14, downloader: QuestDownloading) {
+        self.questTileZoom = questTileZoom
+        self.downloader = downloader
+    }
 }
 
 extension QuestController: QuestControlling {
+    func download(boundingBox: BoundingBox, maxQuestTypesToDownload: Int?, isPriority: Bool) {
+        let tilesRect = boundingBox.enclosingTilesRect(zoom: questTileZoom)
+        
+        downloader.download(tilesRect: tilesRect, maxQuestTypesToDownload: maxQuestTypesToDownload, isPriority: isPriority)
+    }
 }
