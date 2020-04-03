b1ea256a0341a9397e51798696990d5a63144885
wtimme <wtimme@users.noreply.github.com>
2020-04-03 08:39:22 +0200

    Add `download` method to `QuestDownloading`

--- OSMSurveyorFramework/Download/QuestDownloader.swift
+++ OSMSurveyorFramework/Download/QuestDownloader.swift
@@ -9,10 +9,20 @@
 import Foundation
 
 protocol QuestDownloading {
+    /// Download quests in the given `TilesRect`.
+    ///
+    /// - Parameters:
+    ///     - tilesRect: The tiles rect in which to download the quests.
+    ///     - maxQuestTypesToDownload: Download at most the given number of quest types. `nil` for unlimited
+    ///     - isPriority: Whether this shall be a priority download (cancels previous downloads and puts itself in the front)
+    func download(tilesRect: TilesRect, maxQuestTypesToDownload: Int?, isPriority: Bool)
 }
 
 class QuestDownloader {
 }
 
 extension QuestDownloader: QuestDownloading {
+    func download(tilesRect: TilesRect, maxQuestTypesToDownload: Int?, isPriority: Bool) {
+        /// TODO: Implement me.
+    }
 }
