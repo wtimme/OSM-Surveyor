186c0f8c0f291505bf12cc5243c712dccce238cb
wtimme <wtimme@users.noreply.github.com>
2020-04-03 08:10:33 +0200

    Add empty class `QuestController`

---
+++ OSMSurveyorFramework/QuestController.swift
@@ -0,0 +1,18 @@
+//
+//  QuestController.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 03.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+protocol QuestControlling {
+}
+
+final class QuestController {
+}
+
+extension QuestController: QuestControlling {
+}
