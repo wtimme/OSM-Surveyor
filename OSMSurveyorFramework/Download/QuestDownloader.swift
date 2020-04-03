80af02574ada54942f5a803c498fb783605ae50c
wtimme <wtimme@users.noreply.github.com>
2020-04-03 08:35:11 +0200

    Add empty class `QuestDownloader`

---
+++ OSMSurveyorFramework/Download/QuestDownloader.swift
@@ -0,0 +1,18 @@
+//
+//  QuestDownloader.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 03.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+protocol QuestDownloading {
+}
+
+class QuestDownloader {
+}
+
+extension QuestDownloader: QuestDownloading {
+}
