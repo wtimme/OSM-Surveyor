6d28990372509421c81a60cb9c34cef95ae0df17
wtimme <wtimme@users.noreply.github.com>
2020-04-04 23:14:31 +0200

    Add entity `ElementKey`

---
+++ OSMSurveyorFramework/Entities/ElementKey.swift
@@ -0,0 +1,14 @@
+//
+//  ElementKey.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 04.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+struct ElementKey {
+    let elementType: ElementGeometry.ElementType
+    let elementId: Int
+}
