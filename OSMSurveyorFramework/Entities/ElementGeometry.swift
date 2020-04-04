d56f50cf76c132455521e8c06a59b4fc433108a8
wtimme <wtimme@users.noreply.github.com>
2020-04-04 21:42:58 +0200

    Add entity `ElementGeometry`

---
+++ OSMSurveyorFramework/Entities/ElementGeometry.swift
@@ -0,0 +1,27 @@
+//
+//  ElementGeometry.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 04.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+/// Information on the geometry of a quest
+struct ElementGeometry {
+    typealias Polyline = [Coordinate]
+    typealias Polygon = [Coordinate]
+    
+    enum ElementType: String {
+        case node = "NODE"
+        case way = "WAY"
+        case relation = "RELATION"
+    }
+
+    let type: ElementType
+    let elementId: Int
+    let polylines: [Polyline]?
+    let polygons: [Polygon]?
+    let center: Coordinate
+}
