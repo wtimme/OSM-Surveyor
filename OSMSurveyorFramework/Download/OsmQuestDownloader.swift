20f5e0dcee5fc57191fe16796d66cf517dae4494
wtimme <wtimme@users.noreply.github.com>
2020-04-04 13:57:30 +0200

    Add empty class `OsmQuestDownloader`
    This class is supposed to mirror the class of equal name
    from StreetComplete.

---
+++ OSMSurveyorFramework/Download/OsmQuestDownloader.swift
@@ -0,0 +1,18 @@
+//
+//  OsmQuestDownloader.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 04.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+protocol OsmQuestDownloading {
+}
+
+class OsmQuestDownloader {
+}
+
+extension OsmQuestDownloader: OsmQuestDownloading {
+}
