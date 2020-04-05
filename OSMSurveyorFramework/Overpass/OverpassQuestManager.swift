7ea4020dccb04752e0c5e1c5e46bf426a1a83bd6
wtimme <wtimme@users.noreply.github.com>
2020-04-05 16:52:09 +0200

    Add empty class `OverpassQuestManager`

---
+++ OSMSurveyorFramework/Overpass/OverpassQuestManager.swift
@@ -0,0 +1,12 @@
+//
+//  OverpassQuestManager.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 05.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+final class OverpassQuestManager {
+}
