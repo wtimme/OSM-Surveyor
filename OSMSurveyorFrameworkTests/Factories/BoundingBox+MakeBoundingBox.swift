3040328f5429a49426d12e9a28f887ace3b06b16
wtimme <wtimme@users.noreply.github.com>
2020-04-05 19:29:25 +0200

    Add `BoundingBox+MakeBoundingBox` extension for tests

---
+++ OSMSurveyorFrameworkTests/Factories/BoundingBox+MakeBoundingBox.swift
@@ -0,0 +1,17 @@
+//
+//  BoundingBox+MakeBoundingBox.swift
+//  OSMSurveyorFrameworkTests
+//
+//  Created by Wolfgang Timme on 05.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+@testable import OSMSurveyorFramework
+
+extension BoundingBox {
+    static func makeBoundingBox(minimum: Coordinate = Coordinate(latitude: 50.0, longitude: 9.0),
+                                maximum: Coordinate = Coordinate(latitude: 52.0, longitude: 10.0)) -> BoundingBox {
+        return BoundingBox(minimum: minimum, maximum: maximum)
+    }
+}
