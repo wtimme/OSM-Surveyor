d9542d1a653b059b8acd80518a6257e9733425a2
wtimme <wtimme@users.noreply.github.com>
2020-04-05 19:01:38 +0200

    Add mock for `QuestElementProcessing`

---
+++ OSMSurveyorFrameworkTests/Mocks/QuestElementProcessorMock.swift
@@ -0,0 +1,23 @@
+//
+//  QuestElementProcessorMock.swift
+//  OSMSurveyorFrameworkTests
+//
+//  Created by Wolfgang Timme on 05.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+@testable import OSMSurveyorFramework
+
+final class QuestElementProcessorMock {
+    private(set) var didCallProcessElements = false
+    private(set) var processElementsArguments: (elements: [(Element, ElementGeometry?)], boundingBox: BoundingBox, questType: String)?
+}
+
+extension QuestElementProcessorMock: QuestElementProcessing {
+    func processElements(_ elements: [(Element, ElementGeometry?)], in boundingBox: BoundingBox, forQuestOfType questType: String) {
+        didCallProcessElements = true
+        
+        processElementsArguments = (elements, boundingBox, questType)
+    }
+}
