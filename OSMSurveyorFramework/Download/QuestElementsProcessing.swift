9c1eac139bfd8e91aca95c7e36614ec9c02ba214
wtimme <wtimme@users.noreply.github.com>
2020-04-05 18:57:23 +0200

    Add protocol `QuestElementsProcess`

---
+++ OSMSurveyorFramework/Download/QuestElementsProcessing.swift
@@ -0,0 +1,21 @@
+//
+//  QuestElementsProcessing.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 05.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+/// An object that processes downloaded elements for a quest.
+protocol QuestElementProcessing {
+    /// Processes the given elements.
+    /// - Parameters:
+    ///   - elements: The elements to process.
+    ///   - boundingBox: The bounding box that was downloaded.
+    ///   - questType: The type of quest that the elements belong to.
+    func processElements(_ elements: [(Element, ElementGeometry?)],
+                         in boundingBox: BoundingBox,
+                         forQuestOfType questType: String)
+}
