1647469227f64b37aa3dc3998718206a4c4a8821
wtimme <wtimme@users.noreply.github.com>
2020-04-05 17:40:18 +0200

    Add mock for `OverpassQueryExecuting`

---
+++ OSMSurveyorFrameworkTests/Mocks/OverpassQueryExecutorMock.swift
@@ -0,0 +1,23 @@
+//
+//  OverpassQueryExecutorMock.swift
+//  OSMSurveyorFrameworkTests
+//
+//  Created by Wolfgang Timme on 05.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+@testable import OSMSurveyorFramework
+
+final class OverpassQueryExecutorMock {
+    private(set) var didCallExecuteQuery = false
+    private(set) var executeQueryArguments: (query: OverpassQuery, completion: (OverpassQueryResult) -> Void)?
+}
+
+extension OverpassQueryExecutorMock: OverpassQueryExecuting {
+    func execute(query: OverpassQuery, completion: @escaping (OverpassQueryResult) -> Void) {
+        didCallExecuteQuery = true
+        
+        executeQueryArguments = (query, completion)
+    }
+}
