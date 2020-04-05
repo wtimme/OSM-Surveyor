9b8ab1b8e0b1f076fc4da0e839deaa37d75aca71
wtimme <wtimme@users.noreply.github.com>
2020-04-05 19:28:53 +0200

    Add `Node+MakeNode` extension for tests

---
+++ OSMSurveyorFrameworkTests/Factories/Node+MakeNode.swift
@@ -0,0 +1,22 @@
+//
+//  Node+MakeNode.swift
+//  OSMSurveyorFrameworkTests
+//
+//  Created by Wolfgang Timme on 05.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+@testable import OSMSurveyorFramework
+
+extension Node {
+    static func makeNode(id: Int64 = 1,
+                         coordinate: Coordinate = Coordinate(latitude: 53.5, longitude: 9.2),
+                         version: Int64 = 2,
+                         tags: [String: String] = ["amenity": "bench"]) -> Node {
+        return Node(id: id,
+                    coordinate: coordinate,
+                    version: version,
+                    tags: tags)
+    }
+}
