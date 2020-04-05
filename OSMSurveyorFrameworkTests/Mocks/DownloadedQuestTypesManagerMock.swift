8e8fd88695ff37f0e496068ccd50c1ca38bb5df2
wtimme <wtimme@users.noreply.github.com>
2020-04-05 17:00:50 +0200

    Add mock for `DownloadedQuestTypesManaging`

---
+++ OSMSurveyorFrameworkTests/Mocks/DownloadedQuestTypesManagerMock.swift
@@ -0,0 +1,42 @@
+//
+//  DownloadedQuestTypesManagerMock.swift
+//  OSMSurveyorFrameworkTests
+//
+//  Created by Wolfgang Timme on 05.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+@testable import OSMSurveyorFramework
+
+final class DownloadedQuestTypesManagerMock {
+    private(set) var didCallFindDownloadedQuestTypes = false
+    private(set) var findDownloadedQuestTypesArguments: (tilesRect: TilesRect, date: Date)?
+    var findDownloadedQuestTypesReturnValue = [String]()
+    
+    private(set) var didCallMarkQuestTypeAsDownloaded = false
+    private(set) var markQuestTypeAsDownloadedArguments: (tilesRect: TilesRect, questType: String)?
+    var markQuestTypeAsDownloadedError: Error?
+}
+
+extension DownloadedQuestTypesManagerMock: DownloadedQuestTypesManaging {
+    func findDownloadedQuestTypes(in tilesRect: TilesRect, ignoreOlderThan date: Date) -> [String] {
+        didCallFindDownloadedQuestTypes = true
+        
+        findDownloadedQuestTypesArguments = (tilesRect, date)
+        
+        return findDownloadedQuestTypesReturnValue
+    }
+    
+    func markQuestTypeAsDownloaded(tilesRect: TilesRect, questType: String) throws {
+        didCallMarkQuestTypeAsDownloaded = true
+        
+        markQuestTypeAsDownloadedArguments = (tilesRect, questType)
+        
+        if let error = markQuestTypeAsDownloadedError {
+            throw error
+        }
+    }
+    
+    
+}
