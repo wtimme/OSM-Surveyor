f96f860a91633706c9a2a1f300d1975f8b2f4eac
wtimme <wtimme@users.noreply.github.com>
2020-04-05 20:11:40 +0200

    Add mock for `QuestDataManaging`

---
+++ OSMSurveyorFrameworkTests/Mocks/QuestDataManagerMock.swift
@@ -0,0 +1,23 @@
+//
+//  QuestDataManagerMock.swift
+//  OSMSurveyorFrameworkTests
+//
+//  Created by Wolfgang Timme on 05.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+@testable import OSMSurveyorFramework
+
+final class QuestDataManagerMock {
+    private(set) var didCallInsert = false
+    private(set) var insertArguments: (questType: String, elementId: Int, geometry: ElementGeometry)?
+}
+
+extension QuestDataManagerMock: QuestDataManaging {
+    func insert(questType: String, elementId: Int, geometry: ElementGeometry) {
+        didCallInsert = true
+        
+        insertArguments = (questType, elementId, geometry)
+    }
+}
