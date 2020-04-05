a56942021237eac8627cb537d0f810c846dbf97d
wtimme <wtimme@users.noreply.github.com>
2020-04-05 18:59:22 +0200

    Add empty `QuestElementProcessor`

---
+++ OSMSurveyorFramework/Download/QuestElementProcessor.swift
@@ -0,0 +1,18 @@
+//
+//  QuestElementProcessor.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 05.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+final class QuestElementProcessor {
+}
+
+extension QuestElementProcessor: QuestElementProcessing {
+    func processElements(_ elements: [(Element, ElementGeometry?)], in boundingBox: BoundingBox, forQuestOfType questType: String) {
+        /// TODO: Implement me.
+    }
+}
