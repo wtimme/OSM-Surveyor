4951e443dc5297046bf81302b2b666f7731e458f
wtimme <wtimme@users.noreply.github.com>
2020-04-06 21:45:35 +0200

    Add `AddBusStopShelterQuest`

---
+++ OSMSurveyorFramework/Overpass/AddBusStopShelterQuest.swift
@@ -0,0 +1,28 @@
+//
+//  AddBusStopShelterQuest.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 06.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+final class AddBusStopShelterQuest: OverpassQuest {
+    func query(boundingBox: BoundingBox) -> OverpassQuery {
+        let queryWithPlaceholder = """
+        (
+          node["public_transport"="platform"]["shelter"!~".*"]({{bbox}});
+          node["highway"="bus_stop"]["public_transport"!="stop_position"]["shelter"!~".*"]({{bbox}});
+        );
+        """
+        
+        let queryWithBoundingBox = queryWithPlaceholder.replacingOccurrences(of: "{{bbox}}", with: boundingBox.toOverpassBoundingBox())
+        
+        return """
+        [out:json];
+        \(queryWithBoundingBox)
+        out meta;
+        """
+    }
+}
