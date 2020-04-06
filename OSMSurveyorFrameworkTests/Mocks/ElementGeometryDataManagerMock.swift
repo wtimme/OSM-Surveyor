5355ee49436007141843d2f2d5862dc8c0b957ad
wtimme <wtimme@users.noreply.github.com>
2020-04-06 19:27:38 +0200

    Add mock for `ElementGeometryDataManaging`

---
+++ OSMSurveyorFrameworkTests/Mocks/ElementGeometryDataManagerMock.swift
@@ -0,0 +1,23 @@
+//
+//  ElementGeometryDataManagerMock.swift
+//  OSMSurveyorFrameworkTests
+//
+//  Created by Wolfgang Timme on 06.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+@testable import OSMSurveyorFramework
+
+final class ElementGeometryDataManagerMock {
+    private(set) var didCallInsert = false
+    private(set) var insertElement: ElementGeometry?
+}
+
+extension ElementGeometryDataManagerMock: ElementGeometryDataManaging {
+    func insert(_ element: ElementGeometry) {
+        didCallInsert = true
+        
+        insertElement = element
+    }
+}
