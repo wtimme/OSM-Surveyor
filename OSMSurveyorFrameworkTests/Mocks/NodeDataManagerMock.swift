ce09afcbea16fb9db31d39a201e7498dd2e66615
wtimme <wtimme@users.noreply.github.com>
2020-04-06 20:21:36 +0200

    Add mock for `NodeDataManaging`

---
+++ OSMSurveyorFrameworkTests/Mocks/NodeDataManagerMock.swift
@@ -0,0 +1,23 @@
+//
+//  NodeDataManagerMock.swift
+//  OSMSurveyorFrameworkTests
+//
+//  Created by Wolfgang Timme on 06.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+@testable import OSMSurveyorFramework
+
+final class NodeDataManagerMock {
+    private(set) var didCallInsert = false
+    private(set) var insertElement: Node?
+}
+
+extension NodeDataManagerMock: NodeDataManaging {
+    func insert(_ node: Node) {
+        didCallInsert = true
+        
+        insertElement = node
+    }
+}
