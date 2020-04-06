1a5be85d78d83fa3ac7aff584bbb265a98d06e36
wtimme <wtimme@users.noreply.github.com>
2020-04-06 20:29:37 +0200

    Add factory extension on `ElementGeometry` for tests

---
+++ OSMSurveyorFrameworkTests/Factories/ElementGeometry+MakeElementGeometry.swift
@@ -0,0 +1,24 @@
+//
+//  ElementGeometry+MakeElementGeometry.swift
+//  OSMSurveyorFrameworkTests
+//
+//  Created by Wolfgang Timme on 06.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+@testable import OSMSurveyorFramework
+
+extension ElementGeometry {
+    static func makeGeometry(type: ElementType = .node,
+                             elementId: Int = 4,
+                             polylines: [Polyline]? = nil,
+                             polygons: [Polygon]? = nil,
+                             center: Coordinate = Coordinate(latitude: 49.1, longitude: 8.1)) -> ElementGeometry {
+        return ElementGeometry(type: type,
+                               elementId: elementId,
+                               polylines: polylines,
+                               polygons: polygons,
+                               center: center)
+    }
+}
