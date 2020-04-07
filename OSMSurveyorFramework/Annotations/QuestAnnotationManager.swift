f520649b21d111f7871339ebb58a798d8b4ab1a8
wtimme <wtimme@users.noreply.github.com>
2020-04-07 08:51:06 +0200

    Allow for the `QuestAnnotationManagerDelegate` to be `weak`

--- OSMSurveyorFramework/Annotations/QuestAnnotationManager.swift
+++ OSMSurveyorFramework/Annotations/QuestAnnotationManager.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public protocol QuestAnnotationManagerDelegate {
+public protocol QuestAnnotationManagerDelegate: class {
     func addAnnotations(_ annotations: [Annotation])
 }
 
