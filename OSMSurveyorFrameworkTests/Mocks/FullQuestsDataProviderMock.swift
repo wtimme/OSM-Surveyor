56cdfa291b8bc569334ccc04790ebad70a86f559
wtimme <wtimme@users.noreply.github.com>
2020-04-07 09:08:24 +0200

    Add mock for `FullQuestsDataProviding`

---
+++ OSMSurveyorFrameworkTests/Mocks/FullQuestsDataProviderMock.swift
@@ -0,0 +1,38 @@
+//
+//  FullQuestsDataProviderMock.swift
+//  OSMSurveyorFrameworkTests
+//
+//  Created by Wolfgang Timme on 07.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+@testable import OSMSurveyorFramework
+
+final class FullQuestsDataProviderMock {
+    private(set) var didCallFindElementKeysForQuests = false
+    private(set) var findElementKeysForQuestsArguments: (questTypes: [String], boundingBox: BoundingBox)?
+    var elementKeysToReturn = [ElementKey]()
+    
+    private(set) var didCallFindQuests = false
+    private(set) var findQuestsBoundingBox: BoundingBox?
+    var questsToReturn = [(coordinate: Coordinate, questType: String)]()
+}
+
+extension FullQuestsDataProviderMock: FullQuestsDataProviding {
+    func findElementKeysForQuests(ofTypes questTypes: [String], in boundingBox: BoundingBox) -> [ElementKey] {
+        didCallFindElementKeysForQuests = true
+        
+        findElementKeysForQuestsArguments = (questTypes, boundingBox)
+        
+        return elementKeysToReturn
+    }
+    
+    func findQuests(in boundingBox: BoundingBox) -> [(coordinate: Coordinate, questType: String)] {
+        didCallFindQuests = true
+        
+        findQuestsBoundingBox = boundingBox
+        
+        return questsToReturn
+    }    
+}
