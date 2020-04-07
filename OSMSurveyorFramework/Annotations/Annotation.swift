408adf1ce311946160f0317c72d8bda393b51ff9
wtimme <wtimme@users.noreply.github.com>
2020-04-07 08:32:36 +0200

    Add class `Annotation`

---
+++ OSMSurveyorFramework/Annotations/Annotation.swift
@@ -0,0 +1,13 @@
+//
+//  Annotation.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 06.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+public struct Annotation {
+    public let coordinate: Coordinate
+}
