c738f673933919a0d73830d8319bbfb1c7e69016
wtimme <wtimme@users.noreply.github.com>
2020-04-07 08:29:59 +0200

    Add empty class `QuestAnnotationManager`

---
+++ OSMSurveyorFramework/Annotations/QuestAnnotationManager.swift
@@ -0,0 +1,18 @@
+//
+//  QuestAnnotationManager.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 06.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+public protocol QuestAnnotationManaging {
+}
+
+public final class QuestAnnotationManager {
+}
+
+extension QuestAnnotationManager: QuestAnnotationManaging {
+}
