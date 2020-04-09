f3cd862ebd1c988028a62d1f655e93fa9142cee0
wtimme <wtimme@users.noreply.github.com>
2020-04-09 22:26:36 +0200

    Add `TangramAnnotationLayer`

---
+++ OSMSurveyor/Tangram/TangramAnnotationLayer.swift
@@ -0,0 +1,37 @@
+//
+//  TangramAnnotationLayer.swift
+//  OSMSurveyor
+//
+//  Created by Wolfgang Timme on 09.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import OSMSurveyorFramework
+import TangramMap
+
+class TangramAnnotationLayer {
+    private let mapData: TGMapData
+    
+    init(mapData: TGMapData) {
+        self.mapData = mapData
+    }
+}
+
+extension TangramAnnotationLayer: AnnotationLayerProtocol {
+    
+    func setAnnotations(_ annotations: [Annotation]) {
+        guard !annotations.isEmpty else {
+            mapData.clear()
+            return
+        }
+        
+        let features: [TGMapFeature] = annotations.map { singleAnnotation in
+            let coordinate = CLLocationCoordinate2D(latitude: singleAnnotation.coordinate.latitude,
+                                                    longitude: singleAnnotation.coordinate.longitude)
+            
+            return TGMapFeature(point: coordinate, properties: [:])
+        }
+        
+        mapData.setFeatures(features)
+    }
+}
