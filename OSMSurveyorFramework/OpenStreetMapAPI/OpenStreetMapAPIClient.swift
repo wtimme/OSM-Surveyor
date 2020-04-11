6c6f915b7dfbb9183e9459ca2a590858126fca72
wtimme <wtimme@users.noreply.github.com>
2020-04-12 00:29:18 +0200

    Implement fetching of user details with OAuth credentials

--- OSMSurveyorFramework/OpenStreetMapAPI/OpenStreetMapAPIClient.swift
+++ OSMSurveyorFramework/OpenStreetMapAPI/OpenStreetMapAPIClient.swift
@@ -7,6 +7,9 @@
 //
 
 import Foundation
+import Alamofire
+import OAuthSwift
+import OAuthSwiftAlamofire
 
 public struct UserDetails {
     public let username: String
@@ -48,6 +51,9 @@ public final class OpenStreetMapAPIClient {
     private let oAuthConsumerKey: String
     private let oAuthConsumerSecret: String
     
+    /// A temporary `Session` that is being used to retrieve the details of a user when checking the OAuth credentials.
+    private var userDetailsSession: Session?
+    
     // MARK: Initializer
     
     init(baseURL: URL,
@@ -82,7 +88,48 @@ extension OpenStreetMapAPIClient: OpenStreetMapAPIClientProtocol {
     public func userDetails(oAuthToken: String,
                      oAuthTokenSecret: String,
                      completion: @escaping (Result<UserDetails, Error>) -> Void) {
-        /// TODO: Implement me.
+        /// Setup a (temporary) `Session` for fetching the user details.
+        let oAuthSwift = OAuth1Swift(consumerKey: oAuthConsumerKey,
+                                     consumerSecret: oAuthConsumerSecret)
+        oAuthSwift.client.credential.oauthToken = oAuthToken
+        oAuthSwift.client.credential.oauthTokenSecret = oAuthTokenSecret
+        userDetailsSession = Session(interceptor: OAuthSwiftRequestInterceptor(oAuthSwift))
+        
+        let url = baseURL.appendingPathComponent("/api/0.6/user/details")
+        userDetailsSession?.request(url).response(completionHandler: { [weak self] response in
+            guard let self = self else { return }
+            
+            if let error = response.error {
+                completion(.failure(error))
+            } else if
+                let responseData = response.data,
+                let responseString = String(data: responseData, encoding: .utf8),
+                let displayName = self.displayName(from: responseString)
+            {
+                completion(.success(UserDetails(username: displayName)))
+            } else {
+                assertionFailure("No error, but unable to read display name from response.")
+            }
+        })
+    }
+    
+    /// Parses the display_name from the given XML string.
+    ///
+    /// I do realize that this is not an optimal solution, but it works for now. Later, we can add an auto-generated API client for this, and properly parse the XML,
+    /// but for now, this will do.
+    /// - Parameter xmlString: The XML string to parse the display name from.
+    /// - Returns: The display name, if available.
+    private func displayName(from xmlString: String) -> String? {
+        let pattern = #"display_name="([^"]+)"#
+        guard let regex = try? NSRegularExpression(pattern: pattern, options: []) else { return nil }
+        
+        if let match = regex.firstMatch(in: xmlString, options: [], range: NSRange(location: 0, length: xmlString.utf16.count)) {
+            if let range = Range(match.range(at: 1), in: xmlString) {
+                return String(xmlString[range])
+            }
+        }
+        
+        return nil
     }
     
     public func permissions(oAuthToken: String, oAuthTokenSecret: String, completion: @escaping (Result<[Permission], Error>) -> Void) {
