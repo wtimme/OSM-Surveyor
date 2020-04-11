19e47e69bbc39052f5c2c45dfbcd1d4179408c22
wtimme <wtimme@users.noreply.github.com>
2020-04-11 11:34:25 +0200

    Integrate the `SettingsViewModel` into the view controller

--- OSMSurveyor/Settings/SettingsViewController.swift
+++ OSMSurveyor/Settings/SettingsViewController.swift
@@ -12,6 +12,8 @@ final class SettingsViewController: UITableViewController {
     // MARK: Private properties
     private let viewModel: SettingsViewModel
     
+    private let defaultTableViewCellReuseIdentifier = "defaultTableViewCellReuseIdentifier"
+    
     init(style: UITableView.Style = .insetGrouped,
          viewModel: SettingsViewModel = SettingsViewModel()) {
         self.viewModel = viewModel
@@ -30,6 +32,36 @@ final class SettingsViewController: UITableViewController {
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done,
                                                            target: self,
                                                            action: #selector(didTapDoneButton))
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: defaultTableViewCellReuseIdentifier)
+    }
+    
+    // MARK: UITableViewDataSource and delegate
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel.numberOfSections()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRows(in: section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: defaultTableViewCellReuseIdentifier, for: indexPath)
+        
+        if let row = viewModel.row(at: indexPath) {
+            cell.textLabel?.text = row.title
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return viewModel.headerTitleOfSection(section)
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return viewModel.footerTitleOfSection(section)
     }
     
     // MARK: Private method
