c1ef90a72e3bee89d45e30310f292a6814b003ac
wtimme <wtimme@users.noreply.github.com>
2020-04-12 01:27:35 +0200

    Add mock for `KeychainHandling`

---
+++ OSMSurveyorFrameworkMocks/Mocks/KeychainHandlerMock.swift
@@ -0,0 +1,34 @@
+//
+//  KeychainHandlerMock.swift
+//  OSMSurveyorFrameworkMocks
+//
+//  Created by Wolfgang Timme on 12.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+@testable import OSMSurveyorFramework
+
+final class KeychainHandlerMock {
+    var entries = [(username: String, credentials: OAuth1Credentials)]()
+    
+    private(set) var didCallAdd = false
+    private(set) var addArguments: (username: String, credentials: OAuth1Credentials)?
+    
+    private(set) var didCallRemove = false
+    private(set) var usernameToRemove: String?
+}
+
+extension KeychainHandlerMock: KeychainHandling {
+    func add(username: String, credentials: OAuth1Credentials) {
+        didCallAdd = true
+        
+        addArguments = (username, credentials)
+    }
+    
+    func remove(username: String) {
+        didCallRemove = true
+        
+        usernameToRemove = username
+    }
+}
