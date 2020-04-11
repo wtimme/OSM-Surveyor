c02f19180b1a045f3f436de8b054042cfd0927d6
wtimme <wtimme@users.noreply.github.com>
2020-04-11 14:36:44 +0200

    Add `AccountHandlerMock`

---
+++ OSMSurveyorFrameworkMocks/Mocks/AccountHandlerMock.swift
@@ -0,0 +1,17 @@
+//
+//  AccountHandlerMock.swift
+//  OSMSurveyorFrameworkMocks
+//
+//  Created by Wolfgang Timme on 11.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+@testable import OSMSurveyorFramework
+
+final class AccountHandlerMock {
+    var accounts = [Account]()
+}
+
+extension AccountHandlerMock: AccountHandling {
+}
