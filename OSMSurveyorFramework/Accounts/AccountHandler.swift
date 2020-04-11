37a507679a632239a62a37ef8e4988c5897fe109
wtimme <wtimme@users.noreply.github.com>
2020-04-11 14:10:00 +0200

    Add empty `AccountHandler`

---
+++ OSMSurveyorFramework/Accounts/AccountHandler.swift
@@ -0,0 +1,18 @@
+//
+//  AccountHandler.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 11.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+public protocol AccountHandling {
+}
+
+public final class AccountHandler {
+}
+
+extension AccountHandler: AccountHandling {
+}
