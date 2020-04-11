95616f2808e7900f7ff6ab9a2c552dae21838269
wtimme <wtimme@users.noreply.github.com>
2020-04-11 11:44:09 +0200

    Add empty `SettingsCoordinator`

---
+++ OSMSurveyor/Settings/SettingsCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsCoordinator.swift
+//  OSMSurveyor
+//
+//  Created by Wolfgang Timme on 11.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import UIKit
+
+protocol SettingsCoordinatorProtocol: class {
+}
+
+final class SettingsCoordinator {
+}
+
+extension SettingsCoordinator: SettingsCoordinatorProtocol {
+}
