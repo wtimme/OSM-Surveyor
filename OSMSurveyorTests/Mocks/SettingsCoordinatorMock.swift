adb1951cc69fc0c517ef8ab95bf6ecbcf2f46365
wtimme <wtimme@users.noreply.github.com>
2020-04-11 12:07:19 +0200

    Add `SettingsCoordinatorMock`

---
+++ OSMSurveyorTests/Mocks/SettingsCoordinatorMock.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsCoordinatorMock.swift
+//  OSMSurveyorTests
+//
+//  Created by Wolfgang Timme on 11.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+@testable import OSMSurveyor
+
+final class SettingsCoordinatorMock {
+}
+
+extension SettingsCoordinatorMock: SettingsCoordinatorProtocol {
+    func start() {}
+}
