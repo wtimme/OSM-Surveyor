427dedf388e253a583162e0634efb9a9e21305f1
wtimme <wtimme@users.noreply.github.com>
2020-04-11 16:57:36 +0200

    Add empty object `KeychainHandler`

---
+++ OSMSurveyorFramework/Accounts/KeychainHandler.swift
@@ -0,0 +1,18 @@
+//
+//  KeychainHandler.swift
+//  OSMSurveyorFramework
+//
+//  Created by Wolfgang Timme on 11.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+public protocol KeychainHandling {
+}
+
+public class KeychainHandler {
+}
+
+extension KeychainHandler: KeychainHandling {
+}
