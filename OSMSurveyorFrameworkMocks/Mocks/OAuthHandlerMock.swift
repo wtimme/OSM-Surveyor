0f4f35392d15710903f60d9698df94096b2e9189
wtimme <wtimme@users.noreply.github.com>
2020-04-11 19:29:21 +0200

    Add mock for `OAuthHandling`

---
+++ OSMSurveyorFrameworkMocks/Mocks/OAuthHandlerMock.swift
@@ -0,0 +1,35 @@
+//
+//  OAuthHandlerMock.swift
+//  OSMSurveyorFrameworkMocks
+//
+//  Created by Wolfgang Timme on 11.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+@testable import OSMSurveyorFramework
+
+final class OAuthHandlerMock {
+    private(set) var didCallAuthorize = false
+    private(set) var authorizeFromViewController: Any?
+    private(set) var authorizeCompletion: ((OAuthAuthorizationResult) -> Void)?
+    
+    private(set) var didCallHandleURL = false
+    private(set) var urlToHandle: URL?
+}
+
+extension OAuthHandlerMock: OAuthHandling {
+    func authorize(from viewController: Any, completion: @escaping (OAuthAuthorizationResult) -> Void) {
+        didCallAuthorize = true
+        
+        authorizeFromViewController = viewController
+        authorizeCompletion = completion
+    }
+    
+    func handle(url: URL) {
+        didCallHandleURL = true
+        
+        urlToHandle = url
+    }
+    
+}
