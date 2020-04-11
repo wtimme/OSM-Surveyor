0a81e13f457eabf3103bd5272a7d0a10f07e1a53
wtimme <wtimme@users.noreply.github.com>
2020-04-11 10:09:19 +0200

    Add empty `SettingsViewModel`

---
+++ OSMSurveyor/Settings/SettingsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SettingsViewModel.swift
+//  OSMSurveyor
+//
+//  Created by Wolfgang Timme on 11.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+final class SettingsViewModel {
+}
