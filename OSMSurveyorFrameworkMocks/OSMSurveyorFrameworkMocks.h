c254abddfa3704367d2119e49738ffbb1748b6f4
wtimme <wtimme@users.noreply.github.com>
2020-04-11 14:24:38 +0200

    Add framework target "OSMSurveyorFrameworkMocks"
    This target will contain all mocks for "OSMSurveyorFramework",
    since they will also be used by the "OSMSurveyorTests" target.

---
+++ OSMSurveyorFrameworkMocks/OSMSurveyorFrameworkMocks.h
@@ -0,0 +1,19 @@
+//
+//  OSMSurveyorFrameworkMocks.h
+//  OSMSurveyorFrameworkMocks
+//
+//  Created by Wolfgang Timme on 11.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OSMSurveyorFrameworkMocks.
+FOUNDATION_EXPORT double OSMSurveyorFrameworkMocksVersionNumber;
+
+//! Project version string for OSMSurveyorFrameworkMocks.
+FOUNDATION_EXPORT const unsigned char OSMSurveyorFrameworkMocksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OSMSurveyorFrameworkMocks/PublicHeader.h>
+
+
