3b4a62f7575e8eb64a8339bdc8048323ef51f1f6
wtimme <wtimme@users.noreply.github.com>
2020-04-11 14:42:31 +0200

    Add factory extension on `Account` for tests

---
+++ OSMSurveyorFrameworkMocks/Factories/Account+MakeAccount.swift
@@ -0,0 +1,17 @@
+//
+//  Account+MakeAccount.swift
+//  OSMSurveyorFrameworkMocks
+//
+//  Created by Wolfgang Timme on 11.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+@testable import OSMSurveyorFramework
+
+extension Account {
+    static func makeAccount(uuid: UUID = UUID(),
+                            username: String = "jane.doe") -> Account {
+        return Account(uuid: uuid,
+                       username: username)
+    }
+}
