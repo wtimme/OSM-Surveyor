d1185a8697cf58cdab9f6b95a3d0fa0d1738941a
wtimme <wtimme@users.noreply.github.com>
2020-04-11 10:23:55 +0200

    Add `SettingsViewModel.Section`

--- OSMSurveyor/Settings/SettingsViewModel.swift
+++ OSMSurveyor/Settings/SettingsViewModel.swift
@@ -9,4 +9,15 @@
 import Foundation
 
 final class SettingsViewModel {
+    // MARK: Types
+    
+    struct Section {
+        let headerTitle: String?
+        let footerTitle: String?
+        
+        init(headerTitle: String? = nil, footerTitle: String? = nil) {
+            self.headerTitle = headerTitle
+            self.footerTitle = footerTitle
+        }
+    }
 }
