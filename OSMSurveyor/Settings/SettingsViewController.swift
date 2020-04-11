a414c97ab404811664ba9ce164a200b0ba1134e2
wtimme <wtimme@users.noreply.github.com>
2020-04-11 10:13:56 +0200

    Set proper title on `SettingsViewController`

--- OSMSurveyor/Settings/SettingsViewController.swift
+++ OSMSurveyor/Settings/SettingsViewController.swift
@@ -22,4 +22,10 @@ final class SettingsViewController: UITableViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Settings"
+    }
 }
