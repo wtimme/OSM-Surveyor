9a8b1245d893fed3dea7a1ff1e6b35960b116722
wtimme <wtimme@users.noreply.github.com>
2020-04-11 10:07:12 +0200

    Add empty `SettingsViewController`

---
+++ OSMSurveyor/Settings/SettingsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SettingsViewController.swift
+//  OSMSurveyor
+//
+//  Created by Wolfgang Timme on 11.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import UIKit
+
+final class SettingsViewController: UITableViewController {
+}
