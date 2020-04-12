2e5e656a8bfd6164dae7ff2aa476d2ced772b57f
wtimme <wtimme@users.noreply.github.com>
2020-04-12 11:49:13 +0200

    Create mock for `SettingsViewModelDelegate`

---
+++ OSMSurveyorTests/Mocks/SettingsViewModelDelegateMock.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsViewModelDelegateMock.swift
+//  OSMSurveyorTests
+//
+//  Created by Wolfgang Timme on 12.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+@testable import OSMSurveyor
+
+final class SettingsViewModelDelegateMock {
+    private(set) var didCallReloadReloadSection = false
+    private(set) var sectionToReload: Int?
+}
+
+extension SettingsViewModelDelegateMock: SettingsViewModelDelegate {
+    func reloadAccountSection(section: Int) {
+        didCallReloadReloadSection = true
+        
+        sectionToReload = section
+    }
+}
