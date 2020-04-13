3d1a2d0956fedebbf022243ee960040fca40e2b0
wtimme <wtimme@users.noreply.github.com>
2020-04-13 13:52:30 +0200

    Add (empty) category on `UIViewController` for `QuestInteractionDelegate`
    This allows for every view controller to be the delegate for
    the quest interaction.

---
+++ OSMSurveyor/QuestInteraction/UIViewController+QuestInteractionDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewController+QuestInteractionDelegate.swift
+//  OSMSurveyor
+//
+//  Created by Wolfgang Timme on 13.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import UIKit
+import OSMSurveyorFramework
+
+extension UIViewController: QuestInteractionDelegate {
+    public func presentBooleanQuestInterface(question: String,
+                                             completion: @escaping (Bool) -> Void) {
+        /// TODO: Implement me.
+    }
+}
