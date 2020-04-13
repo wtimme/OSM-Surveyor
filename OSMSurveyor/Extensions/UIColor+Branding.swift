96589686024a24b498187ff5c13d84971b74af2c
wtimme <wtimme@users.noreply.github.com>
2020-04-13 19:49:30 +0200

    Add extension on `UIColor` for brand colors

---
+++ OSMSurveyor/Extensions/UIColor+Branding.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Branding.swift
+//  OSMSurveyor
+//
+//  Created by Wolfgang Timme on 13.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var streetCompleteColor: UIColor {
+        UIColor(red: 65/256, green: 65/256, blue: 186/256, alpha: 1)
+    }
+}
