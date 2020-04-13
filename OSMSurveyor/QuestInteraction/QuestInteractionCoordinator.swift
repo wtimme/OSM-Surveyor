f621ed3f945223cefb465e7195aa34a77e11d2d0
wtimme <wtimme@users.noreply.github.com>
2020-04-13 10:46:00 +0200

    Add empty `QuestInteractionCoordinator`

---
+++ OSMSurveyor/QuestInteraction/QuestInteractionCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  QuestInteractionCoordinator.swift
+//  OSMSurveyor
+//
+//  Created by Wolfgang Timme on 13.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import UIKit
+
+protocol QuestInteractionCoordinatorProtocol: class {
+}
+
+final class QuestInteractionCoordinator {
+}
+
+extension QuestInteractionCoordinator: QuestInteractionCoordinatorProtocol {
+}
