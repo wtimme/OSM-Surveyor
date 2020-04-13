7934142907aceafc54f7e993ff9dfe9f9deb08f2
wtimme <wtimme@users.noreply.github.com>
2020-04-13 19:10:59 +0200

    Add `UploadViewModel`

---
+++ OSMSurveyor/Upload/UploadViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  UploadViewModel.swift
+//  OSMSurveyor
+//
+//  Created by Wolfgang Timme on 13.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import UIKit
+
+final class UploadViewModel {
+    // MARK: Private properties
+    
+    private let questId: Int
+    
+    // MARK: Initializer
+    
+    init(questId: Int) {
+        self.questId = questId
+    }
+}
