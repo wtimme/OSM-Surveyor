2baf06cb35be97252ffe2f3a4964fa7e813a9b41
wtimme <wtimme@users.noreply.github.com>
2020-04-13 17:10:18 +0200

    Add documentation

--- OSMSurveyor/QuestInteraction/QuestInteractionDelegate.swift
+++ OSMSurveyor/QuestInteraction/QuestInteractionDelegate.swift
@@ -10,6 +10,10 @@ import Foundation
 
 /// An object that presents the quest interface.
 protocol QuestInteractionDelegate: class {
+    /// Asks the delegate to present an interface for with a question and yes/no answers.
+    /// - Parameters:
+    ///   - question: The question to ask.
+    ///   - completion: Closure to execute when the user has provided an answer.
     func presentBooleanQuestInterface(question: String,
                                       completion: @escaping (Bool) -> Void)
 }
