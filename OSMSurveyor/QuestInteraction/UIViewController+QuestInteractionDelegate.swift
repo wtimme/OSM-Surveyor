ff6fb5a579cd7a72922a65f8993d9d8f2bb598e2
wtimme <wtimme@users.noreply.github.com>
2020-04-13 14:00:27 +0200

    Implement `QuestInteractionDelegate` using `UIAlertController`

--- OSMSurveyor/QuestInteraction/UIViewController+QuestInteractionDelegate.swift
+++ OSMSurveyor/QuestInteraction/UIViewController+QuestInteractionDelegate.swift
@@ -12,6 +12,21 @@ import OSMSurveyorFramework
 extension UIViewController: QuestInteractionDelegate {
     public func presentBooleanQuestInterface(question: String,
                                              completion: @escaping (Bool) -> Void) {
-        /// TODO: Implement me.
+        let alertController = UIAlertController(title: question, message: nil, preferredStyle: .alert)
+        
+        let yesAction = UIAlertAction(title: "Yes", style: .default) { _ in
+            completion(true)
+        }
+        alertController.addAction(yesAction)
+        
+        let noAction = UIAlertAction(title: "No", style: .default) { _ in
+            completion(false)
+        }
+        alertController.addAction(noAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true)
     }
 }
