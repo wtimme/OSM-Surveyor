b137eb894386ab701724cbfaf30069b694a9e7d0
wtimme <wtimme@users.noreply.github.com>
2020-04-15 06:35:12 +0200

    Add protocol `TableViewManaging`
    This protocol combines all methods that the
    `SettingsViewModel` exposes to the `SettingsViewController`.
    By moving them to a dedicated protocol, we can reuse the
    view controller.

---
+++ OSMSurveyor/TableView/TableViewManaging.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewManaging.swift
+//  OSMSurveyor
+//
+//  Created by Wolfgang Timme on 15.04.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import Foundation
+
+protocol TableViewManaging {
+    func numberOfSections() -> Int
+    
+    func numberOfRows(in section: Int) -> Int
+    
+    func row(at indexPath: IndexPath) -> Table.Row?
+    
+    func headerTitleOfSection(_ section: Int) -> String?
+    
+    func footerTitleOfSection(_ section: Int) -> String?
+    
+    func selectRow(at indexPath: IndexPath)
+}
