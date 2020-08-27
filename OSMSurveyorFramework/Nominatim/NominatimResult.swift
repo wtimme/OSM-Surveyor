e7e230e3c7d932d1a3e15980a0b3dbe05c3962ed
wtimme <wtimme@users.noreply.github.com>
2020-08-27 11:22:49 +0200

    Add decoding for `NominatimResult`

--- OSMSurveyorFramework/Nominatim/NominatimResult.swift
+++ OSMSurveyorFramework/Nominatim/NominatimResult.swift
@@ -9,8 +9,28 @@
 import Foundation
 
 /// Result from the Nominatim location search.
-public struct NominatimResult {
+public struct NominatimResult: Decodable {
     public let display_name: String
     public let latitude: Double
     public let longitude: Double
+
+    enum CodingKeys: String, CodingKey {
+        case display_name
+        case latitude = "lat"
+        case longitude = "lon"
+    }
+}
+
+extension NominatimResult {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        display_name = try container.decode(String.self, forKey: .display_name)
+
+        let latitudeAsString = try container.decode(String.self, forKey: .latitude)
+        latitude = (latitudeAsString as NSString).doubleValue
+
+        let longitudeAsString = try container.decode(String.self, forKey: .longitude)
+        longitude = (longitudeAsString as NSString).doubleValue
+    }
 }
