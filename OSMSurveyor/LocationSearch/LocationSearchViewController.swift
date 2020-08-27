e30ff927e10b9d54571896053ee43b6ecef16dda
wtimme <wtimme@users.noreply.github.com>
2020-08-27 09:20:56 +0200

    Add custom view controller for the location search

---
+++ OSMSurveyor/LocationSearch/LocationSearchViewController.swift
@@ -0,0 +1,11 @@
+//
+//  LocationSearchViewController.swift
+//  OSMSurveyor
+//
+//  Created by Wolfgang Timme on 27.08.20.
+//  Copyright © 2020 Wolfgang Timme. All rights reserved.
+//
+
+import UIKit
+
+class LocationSearchViewController: UITableViewController {}
